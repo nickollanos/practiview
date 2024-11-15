f7663a66b0ba0fbb983a0d7ca3996db9aec72d6f
nickollanos <nico26928@hotmail.com>
2024-11-14 19:04:11 -0500

    ..

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('admin.dashboard');
+    return view('aprendiz.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
