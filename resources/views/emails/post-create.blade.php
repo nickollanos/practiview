f92129c88a14f309dda4b278e8b04cb489ec9f9b
AngieBetancur <akbetancurvi@gmail.com>
2024-12-11 00:09:25 -0500

    Se agregan los campos en el modal del calendario de zonas, empresas y aprendices y queda funcionando correctamente

--- resources/views/emails/post-create.blade.php
+++ resources/views/emails/post-create.blade.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Se ha creado correctamente el evento</title>
 </head>
 <body>
     <h1>
-        Correo por aprobar
+        Se ha creado correctamente el evento
     </h1>
     <p>
-        Se ha creado un nuevo post 
+        Ya tienes correctamente creado el evento
     </p>
 </body>
 </html>
